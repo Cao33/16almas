2ad467f970c6aab8155e4c71bc9b1709ba32e67f
Cao33 <pablocao@ucm.es>
2024-06-02 07:34:09 +0200

    Salto personaje
    Se ha añadido un salto y efecto de gravedad. Hay que corregir la comprobacion de cuando se puede saltar

--- src/Entities/Ed.js
+++ src/Entities/Ed.js
@@ -9,8 +9,11 @@ export default class Ed extends Phaser.GameObjects.Sprite
 
         this.a = this.scene.input.keyboard.addKey('A');
         this.d = this.scene.input.keyboard.addKey('D');
+        this.spaceKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.speedX = 0;
-		this.speedY = 0;
+		this.speedY = 0; //la gravedad de la tierra
+        this.seconds = 0; //segundos para usar en el mrua de la gravedad
+        this.jump=false;
 
         this.scene.anims.create({
 			key: 'EdIdleAnim',
@@ -25,21 +28,34 @@ export default class Ed extends Phaser.GameObjects.Sprite
     move()
     {
         if(this.d.isDown){
-            this.speedX=100;
+            this.speedX=200;
         }
         else if(this.a.isDown){
-            this.speedX=-100;
+            this.speedX=-200;
             
         }
         else {
             this.speedX=0;
         }
         
+        if(this.speedY>=0){
+            this.jump=false;
+        }
+
+        if(this.spaceKey.isDown&& !this.jump){
+            this.jump=true;
+            this.seconds=0;
+            this.speedY = -450;
+        }
+        else{
+            this.speedY+=40*this.seconds;
+        }
     }
 
     preUpdate(t,dt)
     {
         super.preUpdate(t,dt);
+        this.seconds+=dt/1000;
         this.move();
         this.body.setVelocity(this.speedX, this.speedY);
     }
