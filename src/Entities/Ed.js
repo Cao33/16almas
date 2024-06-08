c051f21d18d7b0ae9466688c4946e53ecf95abde
Cao33 <pablocao@ucm.es>
2024-06-08 07:13:30 +0200

    Mapa y Ed inicial
    Se ha modificado el mapa para probar las medidas y si se genera bien y se ha modificado la posicion inicial de Ed para que se genere bien. (Se puede hacer con un objeto en phaser que indique el spawn de Ed y me evito meter su posición a mano

--- src/Entities/Ed.js
+++ src/Entities/Ed.js
@@ -6,8 +6,8 @@ export default class Ed extends Phaser.GameObjects.Sprite
         this.scene.add.existing(this);
         this.scene.physics.add.existing(this);
 
-        this.body.setCollideWorldBounds(true);
-        
+        //this.body.setCollideWorldBounds(true);
+
         this.a = this.scene.input.keyboard.addKey('A');
         this.d = this.scene.input.keyboard.addKey('D');
         this.spaceKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
--- src/States/PlayState.js
+++ src/States/PlayState.js
@@ -9,12 +9,16 @@ export default class PlayState extends Phaser.Scene {
     {
         this.load.spritesheet('EdIdle', './assets/Ed/Idle.png', {frameWidth: 32, frameHeight: 32});
         this.load.tilemapTiledJSON('map', './assets/Map/minimap.json');
-        this.load.image('tilesN', './assets/Map/Nature/Tiles.png');
-        this.load.image('tilesM', './assets/Map/Medieval/Tiles.png');
+        this.load.image('NatTiles', './assets/Map/Nature/Tiles.png');
+        this.load.image('MedTiles', './assets/Map/Medieval/Tiles.png');
+        this.load.image('NatProps', './assets/Map/Nature/Props.png');
+        this.load.image('MedProps', './assets/Map/Medieval/Props.png');
     }
 
     create() 
     {
+        const canvasWidth = this.sys.game.config.width;
+        const canvasHeight = this.sys.game.config.height;
         this.debugText = this.add.text(10, 10, 'PlayState', {
             font: '16px Arial',
             fill: '#ffffff'
@@ -29,14 +33,14 @@ export default class PlayState extends Phaser.Scene {
             this.scene.start('PauseState');
         });
 
-        this.Ed = new Ed(this,400,400);
+        this.Ed = new Ed(this,400,879);
 
         this.map = this.make.tilemap({ key: 'map' });
-        const tileset1 = this.map.addTilesetImage('NatureTiles', 'tilesN');
-        const tileset2 = this.map.addTilesetImage('MedievalTiles', 'tilesM');
+        const tileset1 = this.map.addTilesetImage('NatureTiles', 'NatTiles');
+        const tileset2 = this.map.addTilesetImage('MedievalTiles', 'MedTiles');
 
         this.backgroundLayer = this.map.createLayer('Tile Layer 1', tileset1);
-        this.floorLayer = this.map.createLayer('Suelo', tileset2);
+        this.floorLayer = this.map.createLayer('Suelo',tileset2);
         this.floorLayer.setCollisionBetween(0, 9999);
 
         this.cameras.main.startFollow(this.Ed);
